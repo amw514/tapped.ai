f572a9b067643768aea6635d58b5b2fbcb5ae1c4
Ali Munir <alimunirwaris1@gmail.com>
2024-08-28 02:51:48 +0500

    Update VideoDialogue component to use next/image and next/link

--- src/components/VideoDialogue.tsx
+++ src/components/VideoDialogue.tsx
@@ -1,16 +1,14 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
-import HeroVideoDialog from './magicui/hero-video-dialog'
 
 const VideoDialogue = () => {
   return (
     <section className="max-w-5xl mx-auto  flex flex-col justify-center items-center px-7 lg:px-0 relative">
-    <div className="relative rounded-2xl p-1 overflow-hidden">
-      <HeroVideoDialog
-        animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/embed/DPiogp-D4ig"
-        thumbnailSrc="/tapped.ai-img.png"
-        thumbnailAlt="Video thumbnail"
-      />
+    <div className="relative rounded-2xl p-1 overflow-hidden my-6">
+      <Link href="https://app.tapped.ai/">
+        <Image src="/tapped.ai-img.png" alt="tapped ai platform" width={1500} height={1500}/>
+      </Link>
     </div>
   </section>
   )

7bc5a23e58b474359f6004ea11c5acc9b534cd03
ali munir <alimunirwaris1@gmail.com>
2024-09-24 10:44:37 +0500

    fix: update tappedx logo SVG and adjust image size in FloatingNav component

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -25,21 +25,13 @@ export const FloatingNav = ({
   return (
     <div
       className={cn(
-        "flex  w-full sm:w-9/12  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-lg bg-[#1B1B1B] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-80 z-[5000] px-2  py-2  items-center justify-between space-x-4",
+        "flex  w-full sm:w-9/12  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-lg bg-[#1B1B1B] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-80 z-[5000] px-4  py-2  items-center justify-between space-x-4",
         className
       )}
     >
       <div className="block">
-        <Link
-          href="/"
-        >
-          <Image
-            src="/tappedxx.svg"
-            alt="logo"
-            width={160}
-            height={40}
-          />
-
+        <Link href="/">
+          <Image src="/tappedxx.svg" alt="logo" width={120} height={60} />
         </Link>
       </div>
       <div className="flex space-x-4 md:space-x-8 ">
@@ -56,7 +48,6 @@ export const FloatingNav = ({
         ))}
       </div>
       <div className="hidden sm:block">
-        {" "}
         <GettingStartButton />
       </div>
     </div>

edb165682eecf38b4007d2826bee9e873cf05a19
ali munir <alimunirwaris1@gmail.com>
2024-09-24 08:39:56 +0500

    fix: increase maximum width of PitchCard component

--- src/components/PitchCard.tsx
+++ src/components/PitchCard.tsx
@@ -2,7 +2,7 @@ import { HoverEffect } from "./ui/card-hover-effect";
 
 export function PitchCard() {
   return (
-    <div className="max-w-5xl mx-auto px-8">
+    <div className="max-w-7xl mx-auto  px-8">
       <HoverEffect items={projects} />
     </div>
   );
